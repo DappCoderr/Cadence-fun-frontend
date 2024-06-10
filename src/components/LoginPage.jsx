f672c85b5afe65cd47451a2a80194f131fe3eaa4
auspy <vinayak.kshetez0102@gmail.com>
2024-06-10 14:17:11 +0530

    refactor: add common styles

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <main className="bg-primary">
+      <div></div>
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = {
+  red: "#be2e2e",
+  black: "#000000",
+  white: "#ffffff",
+  bg: "#f0f0f0",
+  yellow: "#fdc32e",
+  accent: "#d0effb",
+  primary: "#fea15d",
+  secondary: "#ffc8a1",
+  brown: "#8d7761",
+};
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: colors,
+    },
   },
   plugins: [],
 };
