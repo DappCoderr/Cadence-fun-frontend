dadf7b0e910305c4db2f99ca5593a0caff756dc0
auspy <vinayak.kshetez0102@gmail.com>
2024-06-11 03:44:49 +0530

    feat: logo link

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,17 @@
 import Image from "@/components/Image";
+import { Link } from "react-router-dom";
 export default function Logo({ isVertical, className, ...props }) {
   return (
     <>
-      <div
+      <Link
+        to="/"
         className={`flex ${isVertical ? "flex-col" : "flex-row"} items-center gap-1 ${className}`}
       >
         <Image className={"h-7 w-7"} src={"logo.png"} />
         <p className="" id="logo">
           <span className={isVertical ? "text-white" : ""}> Caden</span>ce Fun
         </p>
-      </div>
+      </Link>
     </>
   );
 }
