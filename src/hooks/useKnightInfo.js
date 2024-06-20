83c49745b23196f1b905ce9def564ff50a96f078
auspy <vinayak.kshetez0102@gmail.com>
2024-06-20 20:03:52 +0530

    feat: dynamic id

--- src/hooks/useKnightInfo.js
+++ src/hooks/useKnightInfo.js
@@ -2,33 +2,50 @@ import { useEffect, useState } from "react";
 import useCurrentUser from "./useCurrentUser";
 import { checkKnightCollection } from "../flow/checkCollection.script";
 import { borrowKnight } from "../flow/borrowKnight.script";
+import { getId } from "../flow/getID.script";
 
 export default function useKnightInfo() {
   const [currentUser] = useCurrentUser();
   const [hasKnight, setHasKnight] = useState(false);
   const [knightInfo, setKnightInfo] = useState({});
+  const [id, setId] = useState(null);
+  const address = currentUser?.addr;
   useEffect(() => {
-    checkKnightCollection(currentUser?.addr).then((result) => {
+    checkKnightCollection(address).then((result) => {
       console.log("result", result);
       setHasKnight(result);
     });
-  }, [currentUser?.addr]);
+  }, [address]);
 
+  const handleBorrowKnight = async (idd = id) => {
+    if (!idd) return;
+    return borrowKnight(address, idd).then((result) => {
+      // console.log("borrowKnight result", result);
+      if (!result) return;
+      setKnightInfo({
+        wins: result.winCount,
+        attack: result.xp,
+        name: result.name,
+        type: result.type,
+      });
+    });
+  };
   useEffect(() => {
     if (hasKnight) {
       console.log("You have a knight!");
-      borrowKnight(currentUser?.addr, 194613558180905).then((result) => {
-        // console.log("borrowKnight result", result);
-        if (!result) return;
-        setKnightInfo({
-          wins: result.winCount,
-          attack: result.xp,
-          name: result.name,
-          type: result.type,
+      let idd = id || null;
+      if (!idd) {
+        // get id
+        return getId(address).then((res) => {
+          console.log("id", res);
+          if (Array.isArray(res) && res.length > 0) idd = res[0];
+          setId(idd);
+          handleBorrowKnight(idd);
         });
-      });
+      }
+      handleBorrowKnight(idd);
     }
-  }, [hasKnight, currentUser?.addr]);
+  }, [hasKnight, address]);
   return {
     knightInfo,
     hasKnight,
