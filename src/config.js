231fdf7d5bdbd269031eeacf660a5892ca845edc
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-07-05 16:00:56 +0530

    Update config file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { config } from "@onflow/fcl";
 
 config({
-  "app.detail.title": "CoinFlip",
+  "app.detail.title": "CadenceFun",
   "app.detail.icon": "https://placekitten.com/g/200/200",
   "accessNode.api": "https://rest-testnet.onflow.org",
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
